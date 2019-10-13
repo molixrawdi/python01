6fa2678f6f0bf94880ae35ea2a713ab2426133cc
Molix <molixrawdi@gmail.com>
2019-10-13 16:03:45 +0100

    Update dailyrepolister.py
    Changed the file to encapsulate daily share with a comment line to indicate that.

--- dailyrepolister.py
+++ dailyrepolister.py
@@ -1,15 +1,21 @@
-import git, github, os, re
-targetdir = "Put your target folder here"
-os.chdir(targetdir)
-repolistpath = "/home/engineer/Documents/Python/repolistlined"
-with open(repolistpath) as targetfile:
-    count = 0
-    while count < 3:
-        content = targetfile.readline()
-        count +=1
-        print(content)
-        
-        
-    
-    
-    
+import os
+
+repolistholder=[]
+os.chdir("/home/engineer/Documents/Python/")
+read_file_path = "/home/engineer/Documents/Python/repolist"
+repo_target_path = "/home/engineer/Documents/Python/repotarget"
+daily_share_divider = "Daily_share_ends_here"
+daily_share = 3 # change this to what ever the daily share is 
+with open(read_file_path, "r") as readfile:
+    repolistholder = readfile.read().split(",")
+    print(repolistholder)
+    print(repolistholder[0:daily_share])
+readfile.close()    
+with open(repo_target_path,"w+") as writefile:  
+    repo_counter_writefile = 0       
+    while repo_counter_writefile < daily_share:
+        writefile.writelines(repolistholder[repo_counter_writefile]+"\n")
+        repo_counter_writefile += 1
+    writefile.writelines(daily_share_divider)    
+    writefile.close()
+
